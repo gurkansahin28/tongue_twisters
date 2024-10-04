2dc219c7894fe4d893ace156e12b9cfc3e19bb1e
gürkan şahin <33648840+gurkansahin28@users.noreply.github.com>
2024-10-05 01:16:48 +0300

    Create tongueTwistersTR.dart

---
+++ tongueTwistersTR.dart
@@ -0,0 +1,9 @@
+const String tongueTwistersTR = "Bir berber bir berbere "
+    "gel beraber bir berber dükkanı açalım demiş."
+    "Kartal kalkar dal sarkar, dal sarkar kartal kalkar."
+    "Şu köşe yaz köşesi, şu köşe kış köşesi"
+    "ortada su şişesi. Şemsi Paşa pasajında sesi büzüşesiceler."
+    "Üç tunç tas has hoş hoşaf."
+    "Şu yoğurdu sarımsaklasak da mı saklasak "
+    "yoksa sarımsaklamasak da mı saklasak. "
+    "Kırk küp kırkının da kulpu kırık küp";
